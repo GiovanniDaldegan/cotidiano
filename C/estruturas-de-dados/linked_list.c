7e2c47d385e5a00f50ddd4bfbd489160841a7860
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-05-11 11:58:18 -0300

    linked list remove at

--- C/estruturas-de-dados/linked_list.c
+++ C/estruturas-de-dados/linked_list.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INVALID_OPERATION 1
+
 struct Node_Int32 {
   int data;
   struct Node_Int32* next;
@@ -31,10 +33,13 @@ struct LinkedList_Int32 InitLinkedList_Int32() {
   return list;
 }
 
-void InsertAtLinkedList_Int32(struct LinkedList_Int32* list,
+int InsertAtLinkedList_Int32(struct LinkedList_Int32* list,
                               int data,
                               int index) {
   struct Node_Int32* newNode = InitNode_Int32(data, NULL);
+
+  if (index > list->size + 1 || index < -list->size)
+    return INVALID_OPERATION;
 
   if (index == 0 || list->size == 0) {
     newNode->next = list->head;
@@ -51,10 +56,7 @@ void InsertAtLinkedList_Int32(struct LinkedList_Int32* list,
       }
     }
     else {
-     if (list->size + index < 0)
-        return;
-
-      while (temp->next != NULL && count < list->size + index) {
+      while (count != list->size + index) {
         temp = temp->next;
         count++;
       }
@@ -65,7 +67,47 @@ void InsertAtLinkedList_Int32(struct LinkedList_Int32* list,
   }
 
   list->size++;
+  return 0;
 }
+
+int RemoveAtLinkedList_Int32(struct LinkedList_Int32* list,
+                              int index) {
+  if (index > list->size - 1 || index < -list->size)
+    return INVALID_OPERATION;
+
+  struct Node_Int32* prt_free = NULL;
+
+  if (index == 0 || index == -list->size) {
+    prt_free = list->head;
+    list->head = list->head->next;
+    free(prt_free);
+  }
+  else {
+    struct Node_Int32* temp1 = list->head;
+    int count = 0;
+
+    if (index > 0) {
+      while (temp1->next == NULL || count != index - 1) {
+        temp1 = temp1->next;
+        count++;
+      }
+    }
+    else {
+      while (count != list->size + index -1) {
+        temp1 = temp1->next;
+        count++;
+      }
+    }
+    
+    prt_free = temp1->next;
+    temp1->next = temp1->next->next;
+    free(prt_free);
+  }
+
+  list->size--;
+  return 0;
+}
+
 
 void PrintLinkedList_Int32(struct LinkedList_Int32 list) {
   struct Node_Int32* temp = list.head;
@@ -82,14 +124,22 @@ void PrintLinkedList_Int32(struct LinkedList_Int32 list) {
 }
 
 
+
 int main() {
   struct LinkedList_Int32 list = InitLinkedList_Int32();
 
-  InsertAtLinkedList_Int32(&list, 0, 0);  // 0
-  InsertAtLinkedList_Int32(&list, 2, 0);  // 2 0
-  InsertAtLinkedList_Int32(&list, 3, 1);  // 2 3 0
-  InsertAtLinkedList_Int32(&list, 123, 1); // 2 123 1 3 0
-  InsertAtLinkedList_Int32(&list, 1, -2); // 2 3 1 0
+  InsertAtLinkedList_Int32(&list, 2, 0);
+  InsertAtLinkedList_Int32(&list, 4, 1);
+  InsertAtLinkedList_Int32(&list, 1, 0);
+  InsertAtLinkedList_Int32(&list, 3, -2);
+  InsertAtLinkedList_Int32(&list, 0, 0);
+  InsertAtLinkedList_Int32(&list, 3, -7);
+  InsertAtLinkedList_Int32(&list, 3, 37);
+
+  RemoveAtLinkedList_Int32(&list, -1);
+  RemoveAtLinkedList_Int32(&list, -2);
+  RemoveAtLinkedList_Int32(&list, -3);
+
   PrintLinkedList_Int32(list);
 
   return 0;
