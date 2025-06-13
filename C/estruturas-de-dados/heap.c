de56443834cf9328529309c292ae8ebc4d205aed
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-13 01:35:01 -0300

    ED - Heap: HeapifyUp()

--- C/estruturas-de-dados/heap.c
+++ C/estruturas-de-dados/heap.c
@@ -16,24 +16,30 @@ Heap* InitHeap(int size)
 
 void HeapifyUp(Heap *heap, int index)
 {
-  if (index > heap->size || index < 0)
+  if (index > heap->size || index < 1)
     return;
 
   int i_parent = (index - 1) / 2;
-  if (&(heap->array[index]) != heap->array)
+  if (heap->array[i_parent] > heap->array[index])
   {
-    if (heap->array[i_parent] > heap->array[index])
-    {
-      int parent = heap->array[i_parent];
-      heap->array[i_parent] = heap->array[index];
-      heap->array[index] = parent;
+    int parent = heap->array[i_parent];
+    heap->array[i_parent] = heap->array[index];
+    heap->array[index] = parent;
 
-      printf("Troca elemento #%d (%d) com #%d (%d)\n",
-             i_parent, parent, index, heap->array[i_parent]);
+    printf("Troca elemento #%d (%d) com #%d (%d)\n",
+           i_parent + 1, parent, index + 1, heap->array[i_parent]);
+
+    int i_parent_parent = (i_parent - 1) / 2;
+    if (i_parent != 0 &&
+        heap->array[i_parent_parent] > heap->array[(i_parent)])
+    {
+      HeapifyUp(heap, i_parent);
     }
   }
 }
 
+void HeapifyDown()
+{}
 
 int main ()
 {
@@ -42,7 +48,7 @@ int main ()
   for (int i = 0; i < 10; i++)
     heap->array[i] = 11 - i;
 
-  HeapifyUp(heap, 1);
+  //HeapifyUp(heap, 1);
   HeapifyUp(heap, 9);
 
   return 0;
